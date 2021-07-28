5d7513389c2ea62a979f9bedce65d3d9e9fbd29e
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-07-28 14:21:53 +0200

    add array song

--- php-ajax-disc/db.php
+++ php-ajax-disc/db.php
@@ -1,1 +1,60 @@
 <!-- Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. -->
+<?php
+
+$db = [
+    [
+        'title' => 'New Jersey',
+        'author' => 'Bon Jovi',
+        'year' => 1988,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+    ],
+    [
+        'title' => "Live at Wembley 86",
+        'author' => "Queen",
+        'year' => 1992,
+        'poster' => "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg"
+    ],
+    [
+        'title' =>  "Ten's Summoner's Tales",
+        'author' => "Sting",
+        'year' => 1993,
+        'poster' => "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg"
+    ],
+    [
+        'title' => 'New Jersey',
+        'author' => "Steve Gadd Band",
+        'year' => 2018,
+        'poster' => "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg"
+    ],
+    [
+        'title' => "Brave new World",
+        'author' => "Iron Maiden",
+        'year' => 2000,
+        'poster' => "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg"
+    ],
+    [
+        'title' => 'One more car, one more raider',
+        'author' => 'Eric Clapton',
+        'year' => 2002,
+        'poster' => "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg"
+    ],
+    [
+        'title' => 'Made in Japan',
+        'author' => "Deep Purple",
+        'year' => 1972,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+    ],
+    [
+        'title' => 'And Justice for All',
+        'author' => 'Metallica',
+        'year' => 1988,
+        'poster' => "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg"
+    ],
+    [
+        'title' => 'Hard Wired',
+        'author' => 'Dave Weckl',
+        'year' => 1994,
+        'poster' => "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg"
+    ]
+    ];
+?>
