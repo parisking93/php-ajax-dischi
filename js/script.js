0a5619f6b0e8fa824c8f0f868bbd20f5aa5d7832
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-07-29 15:10:05 +0200

    log arrai php

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
+let prova = [];
 axios
     .get('api/api-db.php')
     .then(response => {
         // console.log(response.data);
-        let prova = response.data;
+        prova = response.data;
         console.log(prova);
     })
     .catch(error => {
         console.log(error);
     });
+
+for(let i = 0; i <  prova.length; i++) {
+    console.log(prova[i]);
+}
